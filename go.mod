86fc664d0acb07e1d3bcfcf9b35a270b6f55c91c
Dan Khrustalev <77671576+rsmanito@users.noreply.github.com>
2025-02-11 16:08:42 +0200

    test: add tests for register and login

--- go.mod
+++ go.mod
@@ -10,11 +10,13 @@ require (
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/lib/pq v1.10.9
 	github.com/pressly/goose/v3 v3.24.1
+	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.32.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -24,12 +26,16 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.58.0 // indirect
@@ -40,4 +46,5 @@ require (
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ service/auth_test.go
@@ -0,0 +1,179 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/rsmanito/bank-api/models"
+
+	"github.com/rsmanito/bank-api/storage/postgres"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type MockStorage interface{
+		CreateUser(context.Context, postgres.CreateUserParams) error
+		GetUserByEmail(context.Context, string) (postgres.User, error)
+		SaveUserTokens(context.Context, postgres.SaveUserTokensParams) error
+}
+
+type MockStore struct {
+	mock.Mock
+}
+
+func (m *MockStore) CreateUser(ctx context.Context, params postgres.CreateUserParams) error {
+	args := m.Called(ctx, params)
+	return args.Error(0)
+}
+
+func (m *MockStore) SaveUserTokens(ctx context.Context, params postgres.SaveUserTokensParams) error {
+	args := m.Called(ctx, params)
+	return args.Error(0)
+}
+
+func (m *MockStore) GetUserByEmail(ctx context.Context, email string) (postgres.User, error) {
+	args := m.Called(ctx, email)
+	return args.Get(0).(postgres.User), args.Error(1)
+}
+
+func TestRegisterUser_Success(t *testing.T) {
+	mockStore := new(MockStore)
+	svc := &Service{st: mockStore}
+
+	req := &models.RegisterUserRequest{
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "john@example.com",
+		Password:  "password123",
+	}
+
+	mockStore.
+		On("CreateUser", mock.Anything, mock.MatchedBy(func(p postgres.CreateUserParams) bool {
+			var id uuid.UUID
+			if err := id.UnmarshalBinary(p.ID.Bytes[:]); err != nil {
+				return false
+			}
+
+			if p.Email != req.Email {
+				return false
+			}
+
+			err := bcrypt.CompareHashAndPassword(p.Password, []byte(req.Password))
+			return err == nil
+		})).
+		Return(nil).
+		Once()
+
+	err := svc.RegisterUser(context.Background(), req)
+	assert.NoError(t, err)
+
+	mockStore.AssertExpectations(t)
+}
+
+func TestRegisterUser_CreateUserError(t *testing.T) {
+	mockStore := new(MockStore)
+	svc := &Service{st: mockStore}
+
+	req := &models.RegisterUserRequest{
+		FirstName: "Jane",
+		LastName:  "Doe",
+		Email:     "jane@example.com",
+		Password:  "strongpassword",
+	}
+
+	expectedErr := errors.New("create user failed")
+	mockStore.
+		On("CreateUser", mock.Anything, mock.AnythingOfType("postgres.CreateUserParams")).
+		Return(expectedErr).
+		Once()
+
+	err := svc.RegisterUser(context.Background(), req)
+	assert.EqualError(t, err, expectedErr.Error())
+
+	mockStore.AssertExpectations(t)
+}
+
+func TestLoginUser_LoginSuccess(t *testing.T) {
+	mockStore := new(MockStore)
+	svc := &Service{st: mockStore}
+
+	req := &models.LoginUserRequest{
+		Email:    "test@example.com",
+		Password: "stongpassword",
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(req.Password), 8)
+
+
+	mockStore.
+		On("GetUserByEmail", mock.Anything, req.Email).
+		Return(postgres.User{
+			Password: hashedPassword,
+		}, nil).
+		Once()
+	
+	mockStore.
+		On("SaveUserTokens", mock.Anything, mock.AnythingOfType("postgres.SaveUserTokensParams")).
+		Return(nil).
+		Once()
+
+	res, err := svc.LoginUser(context.Background(), req)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, res.Token)
+	assert.NotNil(t, res.RefreshToken)
+
+	mockStore.AssertExpectations(t)
+}
+
+func TestLoginUser_GetUserByEmailError(t *testing.T) {
+	mockStore := new(MockStore)
+	svc := &Service{st: mockStore}
+
+	req := &models.LoginUserRequest{
+		Email:   "test@example.com",
+	}
+
+	expectedErr := errors.New("get user failed")
+
+	mockStore.
+		On("GetUserByEmail", mock.Anything, req.Email).
+		Return(postgres.User{}, errors.New("get user failed")).
+		Once()
+
+	res, err := svc.LoginUser(context.Background(), req)
+
+	assert.EqualError(t, err, expectedErr.Error())
+
+	assert.Nil(t, res)
+}
+
+func TestLoginUser_InvalidCreds(t *testing.T) {
+	mockStore := new(MockStore)
+	svc := &Service{st: mockStore}
+
+	req := &models.LoginUserRequest{
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(req.Password), 8)
+
+	mockStore.
+		On("GetUserByEmail", mock.Anything, req.Email).
+		Return(postgres.User{
+			Password: hashedPassword,
+		}, nil).
+		Once()
+
+	req.Password = "wrongpassword"
+
+	res, err := svc.LoginUser(context.Background(), req)
+
+	assert.EqualError(t, err, models.ErrInvalidCreds.Error())
+
+	assert.Nil(t, res)
+}
